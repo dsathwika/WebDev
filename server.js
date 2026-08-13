9d4ec00318479e4ccd658d64cc64bd9aab8437c6
dsathwika <dsathwika328@gmail.com>
2026-08-13 15:26:39 +0530

    added images to the html page

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 8080;
 app.use(express.static('Frontend'));
 
 app.listen(port, function() {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on 'http://localhost:8080'on port ${port}`);
 });
