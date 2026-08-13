08888be64fa3193a122534bdc7041c6bd8086236
dsathwika <dsathwika328@gmail.com>
2026-08-13 16:31:44 +0530

    added toggle users

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,26 @@
-alert("Welcome to Web Bootcamp")
+//alert("Welcome to Web Bootcamp")
+var users=[
+    {
+        "name":"John Doe",
+        "gender":"Male",
+        "image":"john.png"
+    },
+    {
+        "name":"Jane Doe",
+        "gender":"Female",    
+        "image":"jane.png"
+    }
+]
+
+var curId=0;
+function toggleUser(){
+    curId=(curId+1) % 2;
+
+    var userName=document.getElementById("user-name");
+    var userGender=document.getElementById("user-gender");
+    var userImage=document.getElementById("user-image");
+
+    userName.innerHTML=users[curId].name;
+    userGender.innerHTML=users[curId].gender;
+    userImage.src=users[curId].image;
+}
