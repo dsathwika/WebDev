8cc90e0ab98f6a0443df553d86c8b9e9674dabbf
dsathwika <dsathwika328@gmail.com>
2026-08-13 12:46:23 +0530

    updated server.js file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static('frontend'));
+app.use(express.static('Frontend'));
 
 app.listen(port, function() {
     console.log(`Server is running on port ${port}`);
